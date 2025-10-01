d8f7d2f7bc00f0ee68a4ca49153007cec730f01e
Chester Ramos <cjramos@ucsd.edu>
2025-10-01 16:46:48 -0700

    header 2

--- js/header.js
+++ js/header.js
@@ -1,10 +1,5 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const header = document.getElementById("header");
-    if (header) {
-        header.innerHTML = `
-            <header class="my-header">
-                <div class="logo">MonoLab</div>
-            </header>
-        `;
-    }
-});
+header.innerHTML = `
+    <header class="my-header">
+        <div class="logo">MonoLab</div>
+    </header>
+`;
